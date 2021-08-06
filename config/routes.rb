09e3fb81956fa04d92cc133d98e311c81d7ee392
Susmitha562 <gangupamsusmitha@gmail.com>
2021-08-06 16:40:13 +0530

    pundit and backdoor with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  #use_doorkeeper
   #resources :articles
+  use_doorkeeper do
+    skip_controllers :authorizations, :applications, :authorized_applications
+  end
   devise_for :users
   resources :articles
   root to: "articles#index"
